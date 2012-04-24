dfefc0b255b066db2ab46e85b57516de615b2cc7
Sean Behan <inbox@seanbehan.com>
2012-04-24 16:53:33 -0400

    relative positioning fix by setting width

--- popbox.js
+++ popbox.js
@@ -12,9 +12,9 @@
 
     var methods = {
       open: function(event){
-        event.preventDefault()
+        event.preventDefault();
 
-        var pop = $(this)
+        var pop = $(this);
         var box = $(this).parent().find(settings['box']);
 
         var arrow = box.find(settings['arrow']);
@@ -24,32 +24,33 @@
         arrow_border.css({'left': box.width()/2 - 10});
 
         if(box.css('display') == 'block'){
-          methods.close
+          methods.close();
         } else {
           box.css({'display': 'block', 'top': 10, 'left': ((pop.parent().width()/2) -box.width()/2 )});
         }
       },
 
       close: function(){
-        $(settings['box']).fadeOut("fast")
+        $(settings['box']).fadeOut("fast");
       }
     };
 
     $(document).bind('keyup', function(event){
       if(event.keyCode == 27){
-        methods.close()
+        methods.close();
       }
     });
 
     $(document).bind('click', function(event){
       if(!$(event.target).closest(settings['popbox']).length){
-        methods.close()
+        methods.close();
       }
     });
 
     return this.each(function(){
-      $(settings['pop'], this).bind('click', methods.open)
-      $(settings['pop'], this).parent().find(settings['cancel']).bind('click', methods.close)
+      $(this).css({'width': $(settings['box']).width()}); // Width needs to be set otherwise popbox will not move when window resized.
+      $(settings['pop'], this).bind('click', methods.open);
+      $(settings['pop'], this).parent().find(settings['cancel']).bind('click', methods.close);
     });
   }
 
