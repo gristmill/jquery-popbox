75ad1db7ec1ceeb1f7721271e36c1ce06212559f
Sean Behan <inbox@seanbehan.com>
2012-04-24 11:07:25 -0400

    misc

--- popbox.js
+++ popbox.js
@@ -3,22 +3,17 @@
     var open = function(event){
       var pop = $(this)
       var box = $(this).parent().find('.box');
-      var arrow = box.find('.arrow')
-      var arrow_border = box.find('.arrow-border')
 
-      var w = pop.width()
-      var h = pop.height()
-      var l = pop.position().left + (w/2) - (box.width()/2)
-      var l = pop.width() / 2
-      var t = pop.position().top  + (h*2 - (h/2))
+      var arrow = box.find('.arrow');
+      var arrow_border = box.find('.arrow-border');
 
-      arrow.css({'left': box.width()/2 - 10})
-      arrow_border.css({'left': box.width()/2 - 10})
+      arrow.css({'left': box.width()/2 - 10});
+      arrow_border.css({'left': box.width()/2 - 10});
 
       if(box.css('display')=='block'){
         close()
       } else {
-        box.css({'display': 'block', 'position': 'relative', 'top': '10px', 'left': l});
+        box.css({'display': 'block', 'top': '10px'});
       }
     }
     var close = function(){
