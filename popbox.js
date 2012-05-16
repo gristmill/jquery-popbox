4e6aa4b98eed6e9ee1f29ffc37e8e0be5da1d015
Sean Behan <inbox@seanbehan.com>
2012-05-16 17:06:25 -0400

    prevent event propogation on close. closes #1

--- popbox.js
+++ popbox.js
@@ -47,7 +47,10 @@
     return this.each(function(){
       $(this).css({'width': $(settings['box']).width()}); // Width needs to be set otherwise popbox will not move when window resized.
       $(settings['open'], this).bind('click', methods.open);
-      $(settings['open'], this).parent().find(settings['close']).bind('click', methods.close);
+      $(settings['open'], this).parent().find(settings['close']).bind('click', function(event){
+        event.preventDefault();
+        methods.close();
+      });
     });
   }
 
